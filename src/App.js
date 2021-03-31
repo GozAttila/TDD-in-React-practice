83d517d2298b2f2ca3e3030d70ca868fbbbdd3fd
Gőz Attila <goz.attila.1977@gmail.com>
2021-03-31 15:31:56 +0200

    Create first test

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="app">
-
-    </div>
-  );
+    return (
+        <div className="app">
+            Hello TDD!
+        </div>
+    );
 }
 
 export default App;
